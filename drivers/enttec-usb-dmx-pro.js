7127e6b4b316927ea9e66f076bb718770301f8d3
Thomas Fortier <thomas.fortier@gmail.com>
2017-08-12 22:51:59 -0400

    Convert js to ES6 to be compatible with node version 8

--- drivers/enttec-usb-dmx-pro.js
+++ drivers/enttec-usb-dmx-pro.js
@@ -1,75 +1,76 @@
-"use strict"
+"use strict";
 
-var SerialPort = require("serialport")
+const SerialPort = require("serialport");
 
-var	  ENTTEC_PRO_DMX_STARTCODE = 0x00
-	, ENTTEC_PRO_START_OF_MSG  = 0x7e
-	, ENTTEC_PRO_END_OF_MSG    = 0xe7
-	, ENTTEC_PRO_SEND_DMX_RQ   = 0x06
-	, ENTTEC_PRO_RECV_DMX_PKT  = 0x05
-	;
+const	  ENTTEC_PRO_DMX_STARTCODE = 0x00
+, ENTTEC_PRO_START_OF_MSG  = 0x7e
+, ENTTEC_PRO_END_OF_MSG    = 0xe7
+, ENTTEC_PRO_SEND_DMX_RQ   = 0x06
+, ENTTEC_PRO_RECV_DMX_PKT  = 0x05
+;
 
-function EnttecUSBDMXPRO(device_id, options) {
-	var self = this
-	options = options || {}
-	this.universe = new Buffer(513)
-	this.universe.fill(0)
-	
-	this.dev = new SerialPort(device_id, {
-		'baudrate': 250000,
-		'databits': 8,
-		'stopbits': 2,
-		'parity': 'none'
-	}, function(err) {
-		if(!err) {
-			self.send_universe()
+const EnttecUSBDMXPRO = class EnttecUSBDMXPRO {
+	constructor(device_id, options) {
+		options = options || {};
+		this.universe = new Buffer(512);
+		this.universe.fill(0);
+		
+		this.dev = new SerialPort(device_id, {
+			'baudrate': 250000,
+			'databits': 8,
+			'stopbits': 2,
+			'parity': 'none'
+		}, (err) => {
+			if(!err) {				
+				this.send_universe();
+			}
+		})
+	}
+
+	send_universe () {
+		if(!this.dev.isOpen()) {
+			return;
 		}
-	})
-}
-
-EnttecUSBDMXPRO.prototype.send_universe = function() {
-	if(!this.dev.isOpen()) {
-		return
+		const hdr = Buffer([
+			ENTTEC_PRO_START_OF_MSG,
+			ENTTEC_PRO_SEND_DMX_RQ,
+			 (this.universe.length + 1)       & 0xff,
+			((this.universe.length + 1) >> 8) & 0xff,
+			ENTTEC_PRO_DMX_STARTCODE
+		]);
+	
+		const msg = Buffer.concat([
+			hdr,
+			this.universe,
+			Buffer([ENTTEC_PRO_END_OF_MSG])
+		]);
+		this.dev.write(msg);
 	}
-	var hdr = Buffer([
-		ENTTEC_PRO_START_OF_MSG,
-		ENTTEC_PRO_SEND_DMX_RQ,
-		 (this.universe.length)       & 0xff,
-		((this.universe.length) >> 8) & 0xff,
-		ENTTEC_PRO_DMX_STARTCODE
-	])
 
-	var msg = Buffer.concat([
-		hdr,
-		this.universe.slice(1),
-		Buffer([ENTTEC_PRO_END_OF_MSG])
-	])
-	this.dev.write(msg)
-}
+	start() {}
+	stop()  {}
 
-EnttecUSBDMXPRO.prototype.start = function() {}
-EnttecUSBDMXPRO.prototype.stop = function() {}
-
-EnttecUSBDMXPRO.prototype.close = function(cb) {
-	this.dev.close(cb)
-}
-
-EnttecUSBDMXPRO.prototype.update = function(u) {
-	for(var c in u) {
-		this.universe[c] = u[c]
+	close(cb) {
+		this.dev.close(cb);
 	}
-	this.send_universe()
-}
 
-EnttecUSBDMXPRO.prototype.updateAll = function(v){
-	for(var i = 1; i <= 512; i++) {
-		this.universe[i] = v
+	update(u) {
+		for(const c in u) {
+			this.universe[c] = u[c];
+		}
+		this.send_universe();
 	}
-	this.send_universe()
-}
 
-EnttecUSBDMXPRO.prototype.get = function(c) {
-	return this.universe[c]
+	updateAll(v) {
+		for(const i = 0; i < 512; i++) {
+			this.universe[i] = v;
+		}
+		this.send_universe();
+	}
+
+	get(c) {
+		return this.universe[c];
+	}	
 }
 
 module.exports = EnttecUSBDMXPRO
