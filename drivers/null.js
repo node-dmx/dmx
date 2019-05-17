b2235d591033a222ed5cb7818adfca2e96b5dbb8
Chetan Padia <chetbox@users.noreply.github.com>
2019-05-17 10:10:48 +0100

    add unused options parameter to null driveer

--- drivers/null.js
+++ drivers/null.js
@@ -25,7 +25,7 @@ NullDriver.prototype.close = cb => {
   cb(null);
 };
 
-NullDriver.prototype.update = function (u) {
+NullDriver.prototype.update = function (u, _) {
   for (const c in u) {
     this.universe[c] = u[c];
   }
@@ -34,7 +34,7 @@ NullDriver.prototype.update = function (u) {
   this.emit('update', u);
 };
 
-NullDriver.prototype.updateAll = function (v) {
+NullDriver.prototype.updateAll = function (v, _) {
   for (let i = 1; i <= 512; i++) {
     this.universe[i] = v;
   }
