4fc34e8d809ab5c431cde656fc4c7acba58e5b6f
VK <job@b-vladi.ru>
2023-06-14 12:51:06 +0300

    Fix close socket

--- src/drivers/SerialDriver.js
+++ src/drivers/SerialDriver.js
@@ -1,15 +1,19 @@
 import { SerialPort } from 'serialport';
-import Driver from './Driver.js';
+import Driver, { EVENT_STOP } from './Driver.js'
 
 export const BAUD_RATE = 250000;
 export const DATA_BITS = 8;
 export const STOP_BITS = 2;
+
+export const EVENT_CLOSE = 'close';
 
 export default class SerialDriver extends Driver {
   constructor(options = {}) {
     super(options);
 
     this.ready = true;
+
+    this.on(EVENT_STOP, this.close);
 
     this.serial = new SerialPort({
       path: options.path,
@@ -21,9 +25,8 @@ export default class SerialDriver extends Driver {
   }
 
   close() {
-    return new Promise(resolve => {
-      this.stop();
-      this.serial.close(resolve);
+    this.serial.close((error) => {
+      this.emit(EVENT_CLOSE, error);
     });
   }
 }
